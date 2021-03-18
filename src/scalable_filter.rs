da8562a6b7b136e11338abb05e944a0ac5a99362
Andrea <a.g.baldan@gmail.com>
2021-03-18 19:03:12 +0000

    Add simple set function for scalablefilter

--- src/scalable_filter.rs
+++ src/scalable_filter.rs
@@ -1,13 +1,19 @@
 use crate::filter::BloomFilter;
+use std::error::Error;
+use std::result::Result;
 
+const RATIO: f64 = 0.9;
+
+#[derive(Copy, Clone)]
 pub enum ScaleFactor {
-    SmallScaleSize,
-    LargeScaleSize,
+    SmallScaleSize = 2,
+    LargeScaleSize = 4,
 }
 
 pub struct ScalableBloomFilter {
     initial_capacity: usize,
     filters: Vec<BloomFilter>,
+    fpp: f64,
     scale_factor: ScaleFactor,
 }
 
@@ -16,6 +22,7 @@ impl ScalableBloomFilter {
         Self {
             initial_capacity,
             filters: Vec::new(),
+            fpp: fpp,
             scale_factor: scale_factor,
         }
     }
@@ -28,6 +35,27 @@ impl ScalableBloomFilter {
         self.filters.iter().fold(0, |acc, x| acc + x.size())
     }
 
+    pub fn set(&mut self, bytes: &[u8]) -> Result<bool, Box<dyn Error>> {
+        if self.check(bytes) {
+            return Ok(true);
+        }
+        if let Some(f) = self.filters.last() {
+            if f.size() == f.capacity() {
+                self.add_filter(
+                    self.initial_capacity * self.scale_factor as usize,
+                    self.fpp * RATIO,
+                );
+            }
+        } else {
+            self.add_filter(
+                self.initial_capacity * self.scale_factor as usize,
+                self.fpp * RATIO,
+            );
+        }
+        let filter = self.filters.last().unwrap();
+        filter.set(bytes)
+    }
+
     pub fn check(&self, bytes: &[u8]) -> bool {
         for f in self.filters.iter().rev() {
             if f.check(bytes) {
@@ -35,5 +63,9 @@ impl ScalableBloomFilter {
             }
         }
         return false;
+    }
+
+    fn add_filter(&mut self, capcity: usize, fpp: f64) {
+        self.filters.push(BloomFilter::new(capacity, fpp))
     }
 }
